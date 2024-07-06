f7930b6dfa29ffc141c50e694f4e08e72dd00dcc
Suvam Chanda <Suvam_Chanda@epam.com>
2024-07-06 13:23:28 +0530

    Update code

--- rxjs-course/src/app/app.component.ts
+++ rxjs-course/src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { of, from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'rxjs-course';
+  constructor() {
+    const users = [
+      { id: '1', name: 'Suvam', age: 33 },
+      { id: '2', name: 'Bahni', age: 28 },
+      { id: '3', name: 'Satyaki', age: 1 },
+    ];
+
+    const users$ = of(users);
+    const bodyClick$ = fromEvent(document, 'click');
+
+    users$.subscribe((data) => console.log('user data', data));
+
+    bodyClick$.subscribe((event)=>{
+      console.log('event', event);
+    })
+  }
 }
